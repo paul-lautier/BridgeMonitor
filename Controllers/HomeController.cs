897437a8ba02c9877235ead02a2f17ec6c32eb54
paul-lautier <paul.lautier@ynov.com>
2021-04-03 18:29:29 +0200

    add index with next closing and timer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BridgeMonitor.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace BridgeMonitor.Controllers
 {
@@ -20,12 +22,8 @@ namespace BridgeMonitor.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            var BoatData = GetBoatDataFromApi();
+            return View(BoatData);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -33,5 +31,22 @@ namespace BridgeMonitor.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+             private static List<Closing> GetBoatDataFromApi()
+        {
+            //Création un HttpClient (= outil qui va permettre d'interroger une URl via une requête HTTP)
+            using (var client = new HttpClient())
+            {
+                //Interrogation de l'URL censée me retourner les données
+                var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+                //Récupération du corps de la réponse HTTP sous forme de chaîne de caractères
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+                //Conversion de mon flux JSON (string) en une collection d'objets BikeStation
+                //d'un flux de données vers des objets => Déserialisation
+                //d'objets vers un flux de données => Sérialisation
+                var result = JsonConvert.DeserializeObject<List<Closing>>(stringResult.Result);
+                return result;
+            }
+        }
     }
+
 }
